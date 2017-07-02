de900977f5622b0493f3b4e1c09938b9914556e4
uwage <2fast4glory@protonmail.ch>
2017-07-02 23:45:59 +0300

    альфа

--- src/test/java/webdriver/TinkoffRunTest.java
+++ src/test/java/webdriver/TinkoffRunTest.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import javafx.beans.binding.BooleanExpression;
 import org.junit.After;
@@ -41,60 +42,69 @@ public class TinkoffRunTest {
     }
 
     @Test
-    public void communalServices() throws InterruptedException {
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        driver.navigate().to("https://www.tinkoff.ru/"); // Переходом по адресу https://www.tinkoff.ru/ загрузить стартовую страницу Tinkoff Bank.
-        driver.findElement(By.cssSelector("[data-reactid=\"100\"]")).click(); // Из верхнего меню, нажатием на пункт меню “Платежи“, перейти на страницу “Платежи“.
-        driver.findElement(By.cssSelector("[href=\"/payments/categories/kommunalnie-platezhi/\"]")).click(); // В списке категорий платежей, нажатием на пункт “Коммунальные платежи“, перейти на  страницу выбора поставщиков услуг.
-        if (driver.findElement(By.xpath("//span[contains(text(),'Москве')]")).isDisplayed()) { // Убедиться, что текущий регион – “г. Москва” (в противном случае выбрать регион “г. Москва” из списка регионов).
-
-            System.out.print("Москве");
-
+    public void communalServices() throws Exception {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+// Переходом по адресу https://www.tinkoff.ru/ загрузить стартовую страницу Tinkoff Bank.
+        driver.navigate().to("https://www.tinkoff.ru/");
+// Из верхнего меню, нажатием на пункт меню “Платежи“, перейти на страницу “Платежи“.
+        driver.findElement(By.cssSelector("[data-reactid=\"100\"]")).click();
+// В списке категорий платежей, нажатием на пункт “Коммунальные платежи“, перейти на  страницу выбора поставщиков услуг.
+        driver.findElement(By.cssSelector("[href=\"/payments/categories/kommunalnie-platezhi/\"]")).click();
+// Убедиться, что текущий регион – “г. Москва” (в противном случае выбрать регион “г. Москва” из списка регионов).
+        if (driver.findElement(By.xpath("//span[contains(text(),'Москве')]")).isEnabled() == true) {
+            Thread.sleep(1000);
         } else {
-            Thread.sleep(2000);
             driver.findElement(By.cssSelector("[class=\"ui-link payment-page__title_inner\"]")).click();
-            Thread.sleep(2000);
             driver.findElement(By.xpath("//span[contains(text(),'г. Москва')]")).click();
         }
 
-        Thread.sleep(2000);
-        // Со страницы выбора поставщиков услуг, выбрать 1-ый из списка (Должен быть “ЖКУ-Москва”). Сохранить его наименование (далее “искомый”) и нажатием на соответствующий элемент перейти на страницу оплаты “ЖКУ-Москва“.
-        String compay = driver.findElement(By.cssSelector("[class=\"ui-link ui-menu__link ui-menu__link_logo ui-menu__link_icons ui-menu__link_icons_active\"]")).getText();
-        driver.findElement(By.cssSelector("[class=\"ui-link ui-menu__link ui-menu__link_logo ui-menu__link_icons ui-menu__link_icons_active\"]")).click();
+// Со страницы выбора поставщиков услуг, выбрать 1-ый из списка (Должен быть “ЖКУ-Москва”). Сохранить его наименование
+// (далее “искомый”) и нажатием на соответствующий элемент перейти на страницу оплаты “ЖКУ-Москва“.
+        String compay = driver.findElement(By.cssSelector
+                ("[class=\"ui-link ui-menu__link ui-menu__link_logo ui-menu__link_icons ui-menu__link_icons_active\"]"))
+                .getText();
+        driver.findElement(By.cssSelector
+                ("[class=\"ui-link ui-menu__link ui-menu__link_logo ui-menu__link_icons ui-menu__link_icons_active\"]"))
+                .click();
         String urlmoscow = driver.getCurrentUrl();
-
-        driver.findElement(By.cssSelector("[href=\"/zhku-moskva/oplata/\"]")).click(); // На странице оплаты, перейти на вкладку “Оплатить ЖКУ в Москве“.
+// На странице оплаты, перейти на вкладку “Оплатить ЖКУ в Москве“.
+        driver.findElement(By.cssSelector("[href=\"/zhku-moskva/oplata/\"]")).click();
         Thread.sleep(2000);
 
-        // Выполнить проверки на невалидные значения для обязательных полей: проверить все текстовые сообщения об ошибке (и их содержимое), которые появляются под соответствующим полем ввода в результате ввода некорректных данных.
+// Выполнить проверки на невалидные значения для обязательных полей: проверить все текстовые сообщения об ошибке
+// (и их содержимое), которые появляются под соответствующим полем ввода в результате ввода некорректных данных.
 
-        driver.findElement(By.cssSelector("[data-reactid=\"91\"]")).click(); // Из верхнего меню, нажатием на пункт меню “Платежи“, перейти на страницу “Платежи“.
-        Thread.sleep(2000);
+// Из верхнего меню, нажатием на пункт меню “Платежи“, перейти на страницу “Платежи“.
+        driver.findElement(By.cssSelector("[data-reactid=\"91\"]")).click();
+// В строке быстрого поиска поставщика услуг ввести наименование искомого (ранее сохраненного).
+        driver.findElement(By.cssSelector("[class=\"_3kceY\"]")).sendKeys(compay);
 
-        driver.findElement(By.cssSelector("[class=\"_3kceY\"]")).sendKeys(compay); // В строке быстрого поиска поставщика услуг ввести наименование искомого (ранее сохраненного).
-
-        Thread.sleep(2000);
+// Убедиться, что в списке предложенных провайдеров искомый поставщик первый.
         String compaylist = driver.findElement(By.cssSelector("[class=\"_3WKsv _3KH0m HhZgX _2wQkF\"]")).getText();
-        if(compay.equals(compaylist)) { // Убедиться, что в списке предложенных провайдеров искомый поставщик первый.
-            driver.findElement(By.cssSelector("[class=\"_200uJ\"]")).click(); // Нажатием на элемент, соответствующий искомому, перейти на страницу “Оплатить ЖКУ в Москве“. Убедиться, что загруженная страница та же, что и страница, загруженная в результате шага (5).
+        if(compay.equals(compaylist)) {
+// Нажатием на элемент, соответствующий искомому, перейти на страницу “Оплатить ЖКУ в Москве“. Убедиться,
+// что загруженная страница та же, что и страница, загруженная в результате шага (5).
+            driver.findElement(By.cssSelector("[class=\"_200uJ\"]")).click();
             String urlmoscowlist = driver.getCurrentUrl();
-        }else Thread.sleep(5000);
-        Thread.sleep(2000);
+        }else throw new Exception("Поставщики не совпадают!");
+
+// Из верхнего меню, нажатием на пункт меню “Платежи“, перейти на страницу “Платежи“.
         if(urlmoscow.equals(urlmoscow)){
-            driver.findElement(By.cssSelector("[data-reactid=\"91\"]")).click(); // Из верхнего меню, нажатием на пункт меню “Платежи“, перейти на страницу “Платежи“.
-            Thread.sleep(2000);
-        }else Thread.sleep(5000);
+            driver.findElement(By.cssSelector("[data-reactid=\"91\"]")).click();
+        }else throw new Exception("Загруженная страница не совпадает с шагом 5!");
 
-
-        driver.findElement(By.cssSelector("[href=\"/payments/categories/kommunalnie-platezhi/\"]")).click(); // В списке категорий платежей, нажатием на пункт “Коммунальные платежи“, перейти на  страницу выбора поставщиков услуг.
-        Thread.sleep(2000);
+// В списке категорий платежей, нажатием на пункт “Коммунальные платежи“, перейти на  страницу выбора поставщиков услуг.
+        driver.findElement(By.cssSelector("[href=\"/payments/categories/kommunalnie-platezhi/\"]")).click();
         driver.findElement(By.cssSelector("[class=\"ui-link payment-page__title_inner\"]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//span[contains(text(),'г. Санкт-Петербург')]")).click(); // В списке регионов выбрать “г. Санкт-Петербург”.
-        Thread.sleep(2000);
+// В списке регионов выбрать “г. Санкт-Петербург”.
+        driver.findElement(By.xpath("//span[contains(text(),'г. Санкт-Петербург')]")).click();
+// Убедится, что в списке поставщиков на странице выбора поставщиков услуг отсутствует искомый.
+        if (driver.findElements(By.xpath(compay)).size() == 0) {
+            driver.quit();
+        } else throw new Exception("Найден некорректный элемент!");
     }
-}
+    }
+
 
 
 
