0087e672da438656c14b816c77b90d07d6975d13
uwage <2fast4glory@protonmail.ch>
2017-07-02 15:16:28 +0300

    Менедж драйвер

--- src/test/java/webdriver/TinkoffRunTest.java
+++ src/test/java/webdriver/TinkoffRunTest.java
@@ -1,26 +1,41 @@
 package com.tinkoff.webdriver;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Created by 2fast on 02.07.2017.
  */
+
 public class TinkoffRunTest {
 
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        ChromeDriverManager.getInstance().setup();
+    }
+
     @Before
-    public void startWebDriver(){
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
-
+    public void setupTest() {
+        driver = new ChromeDriver();
     }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
     @Test
-    public void runTest(){
-
-        WebDriver driver = new FirefoxDriver();
-
+    public void communalServices() {
         driver.navigate().to("https://www.tinkoff.ru/");
-        driver.quit();
     }
+
 }
